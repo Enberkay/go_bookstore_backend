d508c6e31dcf9b2e84fb1f54a2b9fa06991e1d03
ciel <enberkaych@gmail.com>
2025-07-31 23:32:38 +0700

    cart model

---
+++ README.md
@@ -0,0 +1,3 @@
+
+เปิด terminal แล้ว รันคำสั่งนี้ใน root โปรเจกต์ (ที่มี go.mod) เพื่อให้ Go ดึง dependency ทั้งหมด
+go mod tidy
---
+++ models/cart.go
@@ -0,0 +1,17 @@
+package models
+
+import "gorm.io/gorm"
+
+type Cart struct {
+	ID     uint `gorm:"primaryKey" json:"id"`
+	UserID uint `json:"user_id"`
+	BookID uint `json:"book_id"`
+	Qty    int  `json:"qty"`
+
+	User User `gorm:"foreignKey:UserID" json:"user"`
+	Book Book `gorm:"foreignKey:BookID" json:"book"`
+}
+
+func MigrateCarts(db *gorm.DB) error {
+	return db.AutoMigrate(&Cart{})
+}
