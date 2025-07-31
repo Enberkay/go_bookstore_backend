d14f5bcac3d5f19b72c6bf390c9e91a8a2227532
ciel <enberkaych@gmail.com>
2025-07-31 23:28:43 +0700

    init project

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func LoadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env")
+	}
+}
+
+func ConnectDB() {
+	db, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to DB")
+	}
+	DB = db
+}
