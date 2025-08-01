603702b5985cb7326b1494155e9ad9891384c4e3
ciel <enberkaych@gmail.com>
2025-08-01 09:22:20 +0700

    fixed cart modules

--- models/cart.go
+++ models/cart.go
@@ -3,10 +3,10 @@ package models
 import "gorm.io/gorm"
 
 type Cart struct {
-	ID     uint `gorm:"primaryKey" json:"id"`
-	UserID uint `json:"user_id"`
-	BookID uint `json:"book_id"`
-	Qty    int  `json:"qty"`
+	ID       uint `gorm:"primaryKey" json:"id"`
+	UserID   uint `json:"user_id"`
+	BookID   uint `json:"book_id"`
+	Quantity int  `json:"Quantity"`
 
 	User User `gorm:"foreignKey:UserID" json:"user"`
 	Book Book `gorm:"foreignKey:BookID" json:"book"`
